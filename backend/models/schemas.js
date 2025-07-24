df1bf8b8cbcee63ea266740af60bc55cf3ac2a79
Jeanette83 <45467119+Jeanette83@users.noreply.github.com>
2025-07-24 10:13:44 -0700

    Update schemas.js

--- backend/models/schemas.js
+++ backend/models/schemas.js
@@ -1,8 +1,9 @@
+//even though there is no actual database for this project I wanted to make the model for it, in case I want
+// to add a backend later
+
+
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-
-
-
 
 const postSchema = new Schema ({
     image: {
